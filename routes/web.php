128f8549dae64a7051056cdf071251faba34bfd1
Dogan Teke <dteke@actineo.de>
2021-10-26 02:28:54 +0200

    adds paypal / credit card payment

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PayPalController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -57,3 +59,8 @@ Route::get('soundcloud', function () {
 Route::post('newtest', function() {
     dd(request()->all());
 });
+
+Route::get('create-transaction', [PayPalController::class, 'createTransaction'])->name('createTransaction');
+Route::get('process-transaction', [PayPalController::class, 'processTransaction'])->name('processTransaction');
+Route::get('success-transaction', [PayPalController::class, 'successTransaction'])->name('successTransaction');
+Route::get('cancel-transaction', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
